448648e5b09f24963edca0ad3576884964ad6dd2
Niklas Fondberg <niklas.fondberg@photowall.se>
2020-07-18 08:54:36 +0200

    Added device and entitites

--- custom_components/easee/sensor.py
+++ custom_components/easee/sensor.py
@@ -3,7 +3,7 @@ Support for Easee charger
 Author: Niklas Fondberg<niklas.fondberg@gmail.com>
 """
 import asyncio
-from typing import List
+from typing import List, Dict
 from datetime import datetime, timedelta
 import logging
 from easee import Charger
@@ -137,9 +137,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         for interval in monitored_days:
             _LOGGER.info("Will measure days: %s", interval)
             sensors.append(
-                ChargerConsumptionSensor(
-                    charger, f"consumption_days_{interval}", int(interval)
-                )
+                ChargerConsumptionSensor(charger, f"consumption_days_{interval}", int(interval))
             )
 
     charger_data = ChargersData(chargers, sensors)
@@ -171,9 +169,7 @@ class ChargersData:
 class ChargerSensor(Entity):
     """Implementation of Easee charger sensor """
 
-    def __init__(
-        self, charger, name, state_key, units, convert_units_func, attrs_keys, icon
-    ):
+    def __init__(self, charger, name, state_key, units, convert_units_func, attrs_keys, icon):
         """Initialize the sensor."""
         self.charger = charger
         self._sensor_name = name
@@ -188,6 +184,21 @@ class ChargerSensor(Entity):
     def name(self):
         """Return the name of the sensor."""
         return f"{DOMAIN}_charger_{self.charger.id}_{self._sensor_name}"
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID."""
+        return f"{self.charger.id}_{self._sensor_name}"
+
+    @property
+    def device_info(self) -> Dict[str, any]:
+        """Return the device information."""
+        return {
+            "identifiers": {(DOMAIN, self.charger.id)},
+            "name": self.charger.name,
+            "manufacturer": "Easee",
+            "model": "Charging Robot",
+        }
 
     @property
     def unit_of_measurement(self):
@@ -237,9 +248,7 @@ class ChargerSensor(Entity):
 
     async def async_update(self):
         """Get the latest data and update the state."""
-        _LOGGER.debug(
-            "ChargerSensor async_update : %s %s", self.charger.name, self._sensor_name
-        )
+        _LOGGER.debug("ChargerSensor async_update : %s %s", self.charger.name, self._sensor_name)
         try:
             self._state = self.get_value_from_key(self._state_key)
             if self._convert_units_func is not None:
@@ -262,6 +271,21 @@ class ChargerConsumptionSensor(Entity):
     def name(self):
         """Return the name of the sensor."""
         return f"{DOMAIN}_charger_{self.charger.id}_{self._sensor_name}"
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID."""
+        return f"{self.charger.id}_{self._sensor_name}"
+
+    @property
+    def device_info(self) -> Dict[str, any]:
+        """Return the device information."""
+        return {
+            "identifiers": {(DOMAIN, self.charger.id)},
+            "name": self.charger.name,
+            "manufacturer": "Easee",
+            "model": "Charging Robot",
+        }
 
     @property
     def unit_of_measurement(self):
@@ -291,9 +315,7 @@ class ChargerConsumptionSensor(Entity):
     async def async_update(self):
         """Get the latest data and update the state."""
         _LOGGER.debug(
-            "ChargerConsumptionSensor async_update : %s %s",
-            self.charger.name,
-            self._sensor_name,
+            "ChargerConsumptionSensor async_update : %s %s", self.charger.name, self._sensor_name,
         )
         now = datetime.now()
         self._state = await self.charger.get_consumption_between_dates(
