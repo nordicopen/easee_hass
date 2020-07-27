e8f04f6881fddf8c4ee464f9cad301fc82755349
tmjo <54450177+tmjo@users.noreply.github.com>
2020-07-27 17:02:32 +0200

    Adding services for charger
    Adding set_dynamicCurrent and set_maxCurrent for charger directly and not only through circuit

--- custom_components/easee/services.py
+++ custom_components/easee/services.py
@@ -30,9 +30,18 @@ SERVICE_CHARGER_SET_BASIC_CHARGEPLAN_SCHEMA = vol.Schema(
     }
 )
 
-SERVICE_SET_CURRENT_SCHEMA = vol.Schema(
+SERVICE_SET_CIRCUIT_CURRENT_SCHEMA = vol.Schema(
     {
         vol.Required(CIRCUIT_ID): cv.positive_int,
+        vol.Required(ATTR_SET_CURRENTP1): cv.positive_int,
+        vol.Optional(ATTR_SET_CURRENTP2): cv.positive_int,
+        vol.Optional(ATTR_SET_CURRENTP3): cv.positive_int,
+    }
+)
+
+SERVICE_SET_CHARGER_CURRENT_SCHEMA = vol.Schema(
+    {
+        vol.Required(CHARGER_ID): cv.string,
         vol.Required(ATTR_SET_CURRENTP1): cv.positive_int,
         vol.Optional(ATTR_SET_CURRENTP2): cv.positive_int,
         vol.Optional(ATTR_SET_CURRENTP3): cv.positive_int,
@@ -100,15 +109,25 @@ SERVICE_MAP = {
         "function_call": "get_basic_charge_plan",
         "schema": SERVICE_CHARGER_ACTION_COMMAND_SCHEMA,
     },
-    "set_dynamic_current": {
-        "handler": "circuit_execute_set_dynamic_current",
+    "set_circuit_dynamic_current": {
+        "handler": "circuit_execute_set_current",
         "function_call": "set_dynamic_current",
-        "schema": SERVICE_SET_CURRENT_SCHEMA,
+        "schema": SERVICE_SET_CIRCUIT_CURRENT_SCHEMA,
     },
-    "set_max_current": {
-        "handler": "circuit_execute_set_max_current",
+    "set_circuit_max_current": {
+        "handler": "circuit_execute_set_current",
         "function_call": "set_max_current",
-        "schema": SERVICE_SET_CURRENT_SCHEMA,
+        "schema": SERVICE_SET_CIRCUIT_CURRENT_SCHEMA,
+    },
+    "set_charger_dynamic_current": {
+        "handler": "charger_execute_set_current",
+        "function_call": "set_dynamic_current",
+        "schema": SERVICE_SET_CHARGER_CURRENT_SCHEMA,
+    },
+    "set_charger_max_current": {
+        "handler": "charger_execute_set_current",
+        "function_call": "set_max_current",
+        "schema": SERVICE_SET_CHARGER_CURRENT_SCHEMA,
     },
 }
 
@@ -136,7 +155,7 @@ async def async_setup_services(hass):
         )
         raise HomeAssistantError("Could not find charger {}".format(charger_id))
 
-    async def circuit_execute_set_dynamic_current(call):
+    async def circuit_execute_set_current(call):
         """Execute a service to Easee circuit. """
         circuit_id = call.data.get(CIRCUIT_ID)
         currentP1 = call.data.get(ATTR_SET_CURRENTP1)
@@ -156,25 +175,25 @@ async def async_setup_services(hass):
         )
         raise HomeAssistantError("Could not find circuit {}".format(circuit_id))
 
-    async def circuit_execute_set_max_current(call):
-        """Execute a service to Easee circuit. """
-        circuit_id = call.data.get(CIRCUIT_ID)
+    async def charger_execute_set_current(call):
+        """Execute a service to Easee circuit for specific charger. """
+        charger_id = call.data.get(CHARGER_ID)
         currentP1 = call.data.get(ATTR_SET_CURRENTP1)
         currentP2 = call.data.get(ATTR_SET_CURRENTP2)
         currentP3 = call.data.get(ATTR_SET_CURRENTP3)
 
         _LOGGER.debug("execute_service:" + str(call.data))
 
-        circuit = next((c for c in circuits if c.id == circuit_id), None)
-        if circuit:
+        charger = next((c for c in chargers if c.id == charger_id), None)
+        if charger:
             function_name = SERVICE_MAP[call.service]
-            function_call = getattr(circuit, function_name["function_call"])
+            function_call = getattr(charger, function_name["function_call"])
             return await function_call(currentP1, currentP2, currentP3)
 
         _LOGGER.error(
-            "Could not find circuit %s", circuit_id,
+            "Could not find charger %s", charger_id,
         )
-        raise HomeAssistantError("Could not find circuit {}".format(circuit_id))
+        raise HomeAssistantError("Could not find charger {}".format(charger_id))
 
     for service in SERVICE_MAP:
         data = SERVICE_MAP[service]
