876fe8ef5fc92c7261058bdad96daa4ff40094bd
Åke Strandberg <ake@strandberg.eu>
2020-11-02 15:41:06 +0100

    Initial support for device automations

--- custom_components/easee/device_trigger.py
+++ custom_components/easee/device_trigger.py
@@ -1,11 +1,12 @@
 """Provides device automations for Easee."""
+import logging
 from typing import List
 
 import voluptuous as vol
 
 from homeassistant.components.automation import AutomationActionType
 from homeassistant.components.device_automation import TRIGGER_BASE_SCHEMA
-from homeassistant.components.homeassistant.triggers import state as state_trigger
+from homeassistant.components.homeassistant.triggers import state
 from homeassistant.const import (
     CONF_DEVICE_ID,
     CONF_DOMAIN,
@@ -17,9 +18,14 @@ from homeassistant.core import CALLBACK_TYPE, HomeAssistant
 from homeassistant.helpers import config_validation as cv, entity_registry
 from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN, EA_DISCONNECTED, EA_CHARGING, EA_COMPLETED, EASEE_STATUS
+from .const import DOMAIN, EA_DISCONNECTED, EA_CHARGING, EA_COMPLETED, EA_ERROR, EA_AWAITING_START, EA_READY_TO_CHARGE
 
-TRIGGER_TYPES = {EA_DISCONNECTED, EA_CHARGING, EA_COMPLETED}
+_LOGGER = logging.getLogger(__name__)
+
+EA_STATES = [EA_AWAITING_START, EA_DISCONNECTED, EA_CHARGING, EA_COMPLETED, EA_ERROR, EA_READY_TO_CHARGE]
+
+# TODO specify your supported trigger types.
+TRIGGER_TYPES = {"charging_started", "charging_completed", "car_disconnected", "awaiting_start", "ready_to_charge", "error"}
 
 TRIGGER_SCHEMA = TRIGGER_BASE_SCHEMA.extend(
     {
@@ -30,22 +36,26 @@ TRIGGER_SCHEMA = TRIGGER_BASE_SCHEMA.extend(
 
 
 async def async_get_triggers(hass: HomeAssistant, device_id: str) -> List[dict]:
-    """List device triggers for Easeee devices."""
+    """List device triggers for Easee devices."""
     registry = await entity_registry.async_get_registry(hass)
     triggers = []
+    _LOGGER.info("async_get_triggers()")
 
     # Get all the integrations entities for this device
     for entry in entity_registry.async_entries_for_device(registry, device_id):
-        if entry.domain != DOMAIN:
+        _LOGGER.debug("Entities: %s - %s", entry.domain, entry.entity_id)
+        if entry.domain != DOMAIN and entry.device_class != "easee_status":
             continue
 
+        # Add triggers for each entity that belongs to this integration
+        _LOGGER.debug("Append triggers to: %s - %s", entry.domain, entry.entity_id)
         triggers.append(
             {
                 CONF_PLATFORM: "device",
                 CONF_DEVICE_ID: device_id,
                 CONF_DOMAIN: DOMAIN,
                 CONF_ENTITY_ID: entry.entity_id,
-                CONF_TYPE: EA_DISCONNECTED,
+                CONF_TYPE: "charging_started",
             }
         )
         triggers.append(
@@ -54,7 +64,7 @@ async def async_get_triggers(hass: HomeAssistant, device_id: str) -> List[dict]:
                 CONF_DEVICE_ID: device_id,
                 CONF_DOMAIN: DOMAIN,
                 CONF_ENTITY_ID: entry.entity_id,
-                CONF_TYPE: EA_CHARGING,
+                CONF_TYPE: "charging_completed",
             }
         )
         triggers.append(
@@ -63,7 +73,34 @@ async def async_get_triggers(hass: HomeAssistant, device_id: str) -> List[dict]:
                 CONF_DEVICE_ID: device_id,
                 CONF_DOMAIN: DOMAIN,
                 CONF_ENTITY_ID: entry.entity_id,
-                CONF_TYPE: EA_COMPLETED,
+                CONF_TYPE: "car_disconnected",
+            }
+        )
+        triggers.append(
+            {
+                CONF_PLATFORM: "device",
+                CONF_DEVICE_ID: device_id,
+                CONF_DOMAIN: DOMAIN,
+                CONF_ENTITY_ID: entry.entity_id,
+                CONF_TYPE: "awaiting_start",
+            }
+        )
+        triggers.append(
+            {
+                CONF_PLATFORM: "device",
+                CONF_DEVICE_ID: device_id,
+                CONF_DOMAIN: DOMAIN,
+                CONF_ENTITY_ID: entry.entity_id,
+                CONF_TYPE: "ready_to_charge",
+            }
+        )
+        triggers.append(
+            {
+                CONF_PLATFORM: "device",
+                CONF_DEVICE_ID: device_id,
+                CONF_DOMAIN: DOMAIN,
+                CONF_ENTITY_ID: entry.entity_id,
+                CONF_TYPE: "error",
             }
         )
 
@@ -79,20 +116,36 @@ async def async_attach_trigger(
     """Attach a trigger."""
     config = TRIGGER_SCHEMA(config)
 
-    if config[CONF_TYPE] == EA_DISCONNECTED:
-        from_state = [state for state in EASEE_STATUS if state != EA_DISCONNECTED]
-        to_state = EA_DISCONNECTED
-    else:
-        from_state = [state for state in EASEE_STATUS if state != EA_CHARGING]
+    # TODO Implement your own logic to attach triggers.
+    # Generally we suggest to re-use the existing state or event
+    # triggers from the automation integration.
+
+    if config[CONF_TYPE] == "charging_started":
+        from_state = [state for state in EA_STATES if state != EA_CHARGING]
         to_state = EA_CHARGING
+    elif config[CONF_TYPE] == "charging_completed":
+        from_state = [state for state in EA_STATES if state != EA_COMPLETED]
+        to_state = EA_COMPLETED
+    elif config[CONF_TYPE] == "car_disconnected":
+        from_state = [state for state in EA_STATES if state != EA_DISCONNECTED]
+        to_state = EA_DISCONNECTED
+    elif config[CONF_TYPE] == "awaiting_start":
+        from_state = [state for state in EA_STATES if state != EA_AWAITING_START]
+        to_state = EA_AWAITING_START
+    elif config[CONF_TYPE] == "ready_to_charge":
+        from_state = [state for state in EA_STATES if state != EA_READY_TO_CHARGE]
+        to_state = EA_READY_TO_CHARGE
+    elif config[CONF_TYPE] == "error":
+        from_state = [state for state in EA_STATES if state != EA_ERROR]
+        to_state = EA_ERROR
 
     state_config = {
-        CONF_PLATFORM: "state",
+        state.CONF_PLATFORM: "state",
         CONF_ENTITY_ID: config[CONF_ENTITY_ID],
-        state_trigger.CONF_FROM: from_state,
-        state_trigger.CONF_TO: to_state,
+        state.CONF_FROM: from_state,
+        state.CONF_TO: to_state,
     }
-    state_config = state_trigger.TRIGGER_SCHEMA(state_config)
-    return await state_trigger.async_attach_trigger(
+    state_config = state.TRIGGER_SCHEMA(state_config)
+    return await state.async_attach_trigger(
         hass, state_config, action, automation_info, platform_type="device"
     )
