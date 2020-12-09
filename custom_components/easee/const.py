ba36c1fdcae5d76adb9a8103dfb766501f591117
theneedyguy <admin@0x01.host>
2020-12-09 15:54:08 +0100

    Do not round value

--- custom_components/easee/const.py
+++ custom_components/easee/const.py
@@ -124,7 +124,7 @@ OPTIONAL_EASEE_ENTITIES = {
         "key": "state.lifetimeEnergy",
         "attrs": [],
         "units": ENERGY_KILO_WATT_HOUR,
-        "convert_units_func": "round_1_dec",
+        "convert_units_func": None,
         "device_class": DEVICE_CLASS_ENERGY,
         "icon": "mdi:counter",
     },
