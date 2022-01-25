4a2a4d2569eecc6ec9cc0ec7da32f57ca23b91e1
Åke Strandberg <ake@strandberg.eu>
2022-01-25 22:59:09 +0100

    Support device diagnostics also

--- custom_components/easee/diagnostics.py
+++ custom_components/easee/diagnostics.py
@@ -1,14 +1,17 @@
 """Diagnostics support for Easee."""
 from __future__ import annotations
+from typing import Any
 
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceEntry
 
 from .const import DOMAIN
 
 TO_REDACT = {CONF_PASSWORD, CONF_USERNAME,}
+TO_REDACT_DATA = {}
 
 
 async def async_get_config_entry_diagnostics(
@@ -19,7 +22,21 @@ async def async_get_config_entry_diagnostics(
     diagnostics_data = {
         "account": async_redact_data(config_entry.data, TO_REDACT),
         "options": async_redact_data(config_entry.options, TO_REDACT),
-        # "data": coordinator.data, # Add more relevant data here
+    }
+
+    return diagnostics_data
+
+async def async_get_device_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry, device: DeviceEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a device."""
+    info = {}
+    info["manufacturer"] = device.manufacturer
+    info["model"] = device.model
+
+    diagnostics_data = {
+        "account": async_redact_data(config_entry.data, TO_REDACT),
+        "data": async_redact_data(info, TO_REDACT_DATA),
     }
 
     return diagnostics_data
