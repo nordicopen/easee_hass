d0a0ba14cc0ae0928a33be202e6bc86c40bf6f3a
Daniel Hjelseth Høyer <github@dahoiv.net>
2023-04-29 09:02:02 +0200

    Async improvements
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- custom_components/easee/sensor.py
+++ custom_components/easee/sensor.py
@@ -22,7 +22,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     controller = hass.data[DOMAIN]["controller"]
     entities = controller.get_sensor_entities()
     async_add_entities(entities)
-    controller.setup_done("sensor")
+    await controller.setup_done("sensor")
 
 
 class ChargerSensor(ChargerEntity, SensorEntity):
