a0998f4c3a90cc4b032b30c7a5364ff31f52001e
Ola Lidholm <olal@olal.se>
2023-06-30 23:41:57 +0200

    bump version

--- custom_components/easee/const.py
+++ custom_components/easee/const.py
@@ -14,7 +14,7 @@ from pyeasee import ChargerStreamData, EqualizerStreamData
 
 DOMAIN = "easee"
 TIMEOUT = 30
-VERSION = "0.9.53"
+VERSION = "0.9.54"
 MIN_HA_VERSION = "2023.4.0"
 CONF_MONITORED_SITES = "monitored_sites"
 MANUFACTURER = "Easee"
