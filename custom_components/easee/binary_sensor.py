43af69cd209f5172188e2f89260e2b6b6a4bfed9
Åke Strandberg <ake@strandberg.eu>
2024-12-08 14:10:23 +0000

    WIP 2

--- custom_components/easee/binary_sensor.py
+++ custom_components/easee/binary_sensor.py
@@ -1,19 +1,60 @@
 """Easee charger binary sensor."""
 
+from dataclasses import dataclass
+from enum import StrEnum
 import logging
+from typing import Final
 
-from homeassistant.components.binary_sensor import BinarySensorEntity
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+    BinarySensorEntityDescription,
+)
+from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 
 from .const import DOMAIN, MANUFACTURER, MODEL_EQUALIZER
+from .controller import Controller
 from .entity import ChargerEntity
 
 _LOGGER = logging.getLogger(__name__)
 
 
+class EaseeType(StrEnum):
+    """Define Easee device classes."""
+
+    CHARGER = "charger"
+    EQUALIZER = "equalizer"
+
+
+@dataclass
+class EaseeBinarySensorDescription(BinarySensorEntityDescription):
+    """Class describing Easee binary sensor entities."""
+
+
+@dataclass
+class EaseeBinarySensorDefinition:
+    """Class for defining binary sensor entities."""
+
+    types: tuple[EaseeType, ...]
+    description: EaseeBinarySensorDescription = None
+
+
+BINARY_SENSORS: Final[tuple[EaseeBinarySensorDefinition, ...]] = (
+    EaseeBinarySensorDefinition(
+        types=[EaseeType.CHARGER],
+        description=EaseeBinarySensorDescription(
+            key="state.cableLocked",
+            entity_category=EntityCategory.DIAGNOSTIC,
+            device_class=BinarySensorDeviceClass.LOCK,
+            translation_key="cable_locked",
+        ),
+    )
+)
+
+
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up binary sensor platform."""
-    controller = hass.data[DOMAIN]["controller"]
+    controller: Controller = hass.data[DOMAIN]["controller"]
     entities = controller.get_binary_sensor_entities()
     async_add_entities(entities)
     await controller.setup_done("binary_sensor")
