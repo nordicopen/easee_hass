9d248e113842e4f6739733e7cd1387b584aa7ca1
Ola Lidholm <olal@olal.se>
2026-07-21 11:59:34 +0200

    Bump revision

--- custom_components/easee/const.py
+++ custom_components/easee/const.py
@@ -16,7 +16,7 @@ from homeassistant.helpers.entity import EntityCategory
 
 DOMAIN = "easee"
 TIMEOUT = 30
-VERSION = "0.9.73"
+VERSION = "0.9.74"
 MIN_HA_VERSION = "2025.7.0"
 CONF_MONITORED_SITES = "monitored_sites"
 MANUFACTURER = "Easee"
